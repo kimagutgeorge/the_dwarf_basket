6995e21791dbd8e1e6a67b9e37b0486fc232c8c8
George Kimagut <geojimagut@gmail.com>
2025-03-01 13:58:47 +0300

    <full home page>

--- src/views/users/HomeView.vue
+++ src/views/users/HomeView.vue
@@ -251,9 +251,98 @@
     </div>
    </div>
    <!-- reviews -->
-   <div class="flex-container home-cat reviews">
-
+   <div class="flex-container home-cat reviews f-width">
+    <div class="half">
+      <div class="review-card">
+        <div class="review-card-top fixed-flexed">
+          <img src="../../assets/logo/profile.png"><span>George Kimagut</span>
+        </div>
+        <div class="review-body">
+          <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <div class="fixed-flexed">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+        </div>
+      </div>
+      <div class="review-pagination fixed-flexed">
+        <button class="paginate-btn"><i class="fa-solid fa-angle-left"></i></button>
+        <button class="paginate-btn"><i class="fa-solid fa-angle-right"></i></button>
+      </div>
+    </div>
+    <div class="half">
+      <div class="review-topic h-card">
+          <div class="h-card-bg p-absolute">
+            <div class="h-card-inner-text">
+              <p>"</p>
+              <p>What our </p>
+              <p>customers </p>
+              <p>are saying?</p>
+              
+            </div>
+          </div>
+          <div class="h-card-bg-1 p-absolute"></div>
+      </div>
+    </div>
    </div>
+   <!-- footer -->
+   <div class="flex-container home-cat footer f-width">
+    <div class="col-30">
+      <div class="col-body">
+        <div class="fixed-flexed">
+          <img src="../../assets/logo/g11.png" class="logo-img"> <span>Dwarf Basket</span>
+        </div>
+        <p>Our passion for design with straight speaking strategy
+          to create thoughtful and engaging solutions, ensures we
+          serve our clients – big and small – year after year.</p>
+          <div class="fixed-flexed footer-icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-x-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin-in"></i>
+          </div>
+      </div>
+    </div>
+    <div class="col-20">
+      <div class="col-body">
+        <h4>About Us</h4>
+        <div class="col-body-link">
+          <li><router-link to="/about-us" class="footer-link fixed-flexed"><i class="fa-solid fa-angle-right"></i> About</router-link></li>
+          <li><router-link to="/products" class="footer-link fixed-flexed"><i class="fa-solid fa-angle-right"></i> Products</router-link></li>
+          <li><router-link to="/services" class="footer-link fixed-flexed"><i class="fa-solid fa-angle-right"></i> Services</router-link></li>
+          <li><router-link to="/contact-us" class="footer-link fixed-flexed"><i class="fa-solid fa-angle-right"></i> Contact</router-link></li>
+        </div>
+      </div>
+    </div>
+    <div class="col-20">
+      <div class="col-body">
+        <h4>Get In Touch</h4>
+        <div class="col-body-link">
+          <li class="touch fixed-flexed"><i class="fa-solid fa-location-pin"></i> Rhapta Heights, 2nd Floor, Rhapta Rd, Westlands</li>
+          <li class="touch fixed-flexed"><i class="fa-solid fa-phone"></i> +254 759 200 998</li>
+          <li class="touch fixed-flexed"><i class="fa-solid fa-envelope"></i> info@thedwarfbasket.com</li>
+        </div>
+      </div>
+    </div>
+    <div class="col-30">
+      <div class="col-body">
+        <h4>Categories</h4>
+        <div class="col-body-link">
+          <li class="touch">Tags</li>
+          <li class="touch">ThermoRoll Labels</li>
+          <li class="touch">Sustainable Labels</li>
+          <li class="touch">Office Stationary</li>
+        </div>
+      </div>
+   </div>
+  </div>
+  <!-- footer bottom -->
+  <div class="footer-bottom f-width">
+    <p>Copyright &copy; 2025, The Dwarf Basket. All Rights Reserved</p>
+  </div>
 </template>
 
 <script>
